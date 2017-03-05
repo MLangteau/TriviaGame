4fa65bc56233467ac4b25caca8ec08f7ee064c9e
MLangteau <langteauM@gmail.com>
2017-03-04 22:33:00 -0500

    time and questions are good

---
+++ assets/javascript/app.js
@@ -0,0 +1,154 @@
+var questionQuest;
+var questionCount = 0;
+var timer;
+
+//  Game Questions Object
+
+  var gameQuest = [{
+  					question: "What is one of the best know catch phrases from this movie?",
+		            answers:["Gotta keep moving","Don't stop now","Just keep swimming","I can't remember"],
+	  	//			url: "http://www.grahamowengallery.com/forum/mallard.jpg",
+	  				correctAnswer: "Just keep swimming"
+	  	//			url: "../images/...jpg",			
+	  				},
+					
+					{
+					question: "What type of a fish is Nemo?",
+	  				answers:["Blowfish","Clownfish","Pufferfish","Striped fish"],
+	  //				url: "https://i.ytimg.com/vi/_z-1fTlSDF0/maxresdefault.jpg",
+	  				correctAnswer: "Clownfish"
+	  				},
+					
+					{
+					question: "How old is Crush, the Turtle?",
+	  				answers:["102","150","40","99"],
+	  //				url: "http://www.grahamowengallery.com/forum/mallard.jpg",
+	  				correctAnswer: "150",
+	  				},
+					
+					{
+					question: "From what disorder does Dory suffer?",
+	  				answers:["Borderline personality disorder","Dementia","Anterograde Amnesia","Autism"],
+	  //				url: "http://www.grahamowengallery.com/forum/mallard.jpg",
+	  				correctAnswer: "Anterograde Amnesia",
+	  				},
+					
+					{
+					question: "What is the safest way for Dory and Marlin to get past the maze of Jellyfish",
+	  				answers:["Over","Under","Through","Around"],
+	  //				url: "http://www.grahamowengallery.com/forum/mallard.jpg",
+	  				correctAnswer: "Over",
+	  				},
+					
+					{
+					question: "What is Anterograde Amnesia?",
+	  				answers:["Inability to swim for a long time","Inability to keep friends","Inability to swim straight","Inability to form new memories"],
+	  //				url: "http://www.grahamowengallery.com/forum/mallard.jpg",
+	  				correctAnswer: "Inability to form new memories"
+	  				},
+
+					{
+					question: "Which one of these can foster memory retention for those who suffer from Anterograde Amnesia?",
+	  				answers:["A positive environment","Social support","Familiarity","All of the above"],
+	  //				url: "http://www.grahamowengallery.com/forum/mallard.jpg",
+	  				correctAnswer: "All of the above",
+	  				},
+
+					{
+					question: "Why was Nemo's fin smaller than the other?",
+	  				answers:["Birth defect","Ran into the side of a boat","Attacked while still in the egg","Clipped by a shark"],
+	  //				url: "http://www.grahamowengallery.com/forum/mallard.jpg",
+	  				correctAnswer: "Attacked while still in the egg",
+	  				},
+
+					{
+					question: "What address was Dory and Marlin were looking for?",
+	  				answers:["44 Winding Way","13 Shark Drive","52 Reed Road","42 Wallaby Way"],
+	  //				url: "http://www.grahamowengallery.com/forum/mallard.jpg",
+	  				correctAnswer: "42 Wallaby Way",
+	  
+	  				},
+	  									{
+					question: "Who ate Nemo's mother, Coral, and all but one of her eggs?",
+	  				answers:["Jellyfish","Barracuda","A shark","A whale"],
+	  //				url: "https://i.ytimg.com/vi/_z-1fTlSDF0/maxresdefault.jpg",
+	  				correctAnswer: "Barracuda"
+	  				}];
+
+//Declare the timer variable above the game object
+
+var game = {
+  correct: 0,
+  incorrect: 0,
+// In the top of game{}, set the counter to be whatever value want
+//     SHOULD BE 120  counter: 120,
+
+	counter: 25,
+
+//Here we have the method for doing the actual counting down 
+  countdown: function() {
+    game.counter--;
+//    console.log("game.counter: inside of countdown function: " + game.counter);
+//    $("#counter-number").html(game.counter);
+    $("#counter-number").text(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+//This calls a game over function
+      game.done();
+    }  // end of if
+  },  // end of countdown function
+
+  start: function() {
+//Inside start Function we set timer by calling the countdown method\
+    
+    timer = setInterval(game.countdown, 1000);
+//    console.log("here is the timer")
+//    console.log("timer: " + timer);
+//  $("#timerDisplay").html("");
+  	$("#timerDisplay").prepend("<h2>Time Remaining: <span id='counter-number'></span> Seconds</h2>");
+//	$("#questionAnswerDiv").hide();
+
+	$("#start").remove(); //remove start button; no longer needed (hide if needed)
+
+	for (var i=0; i<gameQuest.length; i++){
+//		console.log("gameQuest[i].question BOZO ", gameQuest[i].question);
+
+	   $("#questionAnswerDiv").append("<h2>" + gameQuest[i].question + "</h2>");
+
+		for (var j=0; j<gameQuest[i].answers.length; j++){
+		  $("#questionAnswerDiv").append("<input type='radio' name='question-" + i +
+        "' value='" + gameQuest[i].answers[j] + "''>" + gameQuest[i].answers[j]);
+		}; // end of j loop
+	};   // end of i loop
+
+	game.countdown();
+
+  },  // end of start function
+
+// clear the interval in the end
+
+	done: function() {
+		console.log("Timer in done function before clearInterval: " + timer);
+		clearInterval(timer);
+		console.log("Timer in done function after clearInterval: " + timer);
+	}
+
+  };  // end of game object
+
+//  This code will run as soon as the page loads.
+$(document).ready(function() {
+//                              ml or jQuery(function($) {thos 4 in this})
+  //  Click events are done for us:
+//  $("#lap").click(stopwatch.recordLap);
+//  $("#stop").click(stopwatch.stop);
+ // $("#reset").click(stopwatch.reset);
+
+/// THIS IS WHAT WORKS  
+$("#start").click(game.start);
+
+// dot and variable notation (most of the time files will be with JSON or object)
+
+	console.log(game);
+
+
+}); // end of document.ready
